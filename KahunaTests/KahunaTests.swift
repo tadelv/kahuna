4460ab936ff9dfd56c525d11f47ce16c8555722f
Vid Tadel <tadelv@gmail.com>
2014-12-04 13:22:43 +0100

    Initial Commit

---
+++ KahunaTests/KahunaTests.swift
@@ -0,0 +1,36 @@
+//
+//  KahunaTests.swift
+//  KahunaTests
+//
+//  Created by Vid Tadel on 04/12/14.
+//  Copyright (c) 2014 Vid Tadel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KahunaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
