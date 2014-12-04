0358e6c49f68aa0e4a2e25409fb0729c46556483
Vid Tadel <tadelv@gmail.com>
2014-12-04 20:58:34 +0100

    add titles to view controllers, center the history button in group detail view controller and set the group detail view controller title to match group name

--- Kahuna/GroupDetailViewController.swift
+++ Kahuna/GroupDetailViewController.swift
@@ -11,7 +11,6 @@ import CoreData
 
 class GroupDetailViewController: UITableViewController, NSFetchedResultsControllerDelegate, AddUserControllerDelegate {
 
-	var detailViewController: DetailViewController? = nil
 	var managedObjectContext: NSManagedObjectContext? = nil
 	var detailItem: Group? {
 		didSet {
@@ -35,19 +34,13 @@ class GroupDetailViewController: UITableViewController, NSFetchedResultsControll
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-//		self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
 		let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
 		self.navigationItem.rightBarButtonItem = addButton
-		if let split = self.splitViewController {
-			let controllers = split.viewControllers
-			self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
-		}
 	}
 
 	func configureView() {
-
+        self.title = self.detailItem!.name
 	}
 
 	override func didReceiveMemoryWarning() {
